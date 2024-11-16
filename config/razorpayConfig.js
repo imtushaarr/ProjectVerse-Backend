de318b7dd04d90097be1bcc4d9379f22fb74b2a9
Tushar Gupta <tusharguptagps@gmail.com>
2024-11-17 02:20:06 +0530

    Update razorpayConfig.js

--- config/razorpayConfig.js
+++ config/razorpayConfig.js
@@ -2,8 +2,8 @@
 const Razorpay = require('razorpay');
 
 const razorpayInstance = new Razorpay({
-  key_id: 'rzp_live_afo9xw5T1MT8gE', // Replace with your Razorpay key
-  key_secret: 'sXh6989pHPrkTZwQUYz25N61', // Replace with your Razorpay secret
+  key_id: 'ENTER_YOUR_KEYID', // Replace with your Razorpay key
+  key_secret: 'ENTER_YOU_KEY_ID', // Replace with your Razorpay secret
 });
 
 module.exports = razorpayInstance;
